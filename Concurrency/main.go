c01e7dbbdc677dd5c92c81a9d8603b17b15bb591
sieunhangao <friendd1996@gmail.com>
2018-08-16 02:41:29 +0700

    total conflix

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -2,31 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
 
 var vg = sync.WaitGroup{}
+var total = 0
 
-func foo() {
-	for i := 0; i < 10; i++ {
-		fmt.Printf("foo %d \n", i)
-		time.Sleep(time.Millisecond * 500)
-	}
-	vg.Done()
-}
-
-func bar() {
-	for i := 0; i < 10; i++ {
-		fmt.Printf("bar %d \n", i)
-		time.Sleep(time.Millisecond * 500)
+func upNumber() {
+	for i := 0; i < 20; i++ {
+		x := total
+		x++
+		time.Sleep(time.Duration(rand.Intn(30)) * time.Millisecond)
+		total = x
 	}
 	vg.Done()
 }
 
 func main() {
 	vg.Add(2)
-	go foo()
-	go bar()
+	go upNumber()
+	go upNumber()
 	vg.Wait()
+	// expect 40
+	fmt.Println(total)
 }
