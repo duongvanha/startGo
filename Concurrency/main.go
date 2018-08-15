c54217f9310d0856d08937cda23f053893f679fa
sieunhangao <friendd1996@gmail.com>
2018-08-16 02:25:06 +0700

    concurrency example

---
+++ Concurrency/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var vg = sync.WaitGroup{}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("foo %d \n", i)
+		time.Sleep(time.Millisecond * 500)
+	}
+	vg.Done()
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("bar %d \n", i)
+		time.Sleep(time.Millisecond * 500)
+	}
+	vg.Done()
+}
+
+func main() {
+	vg.Add(3)
+	go foo()
+	go bar()
+	vg.Wait()
+}
